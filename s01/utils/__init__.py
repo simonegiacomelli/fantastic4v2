26b0645e84ce6b6f26f766abbd13a0568a418080
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-05-27 10:43:56 +0200

    lazy init

--- s01/utils/__init__.py
+++ s01/utils/__init__.py
@@ -1,18 +1,20 @@
 import os
 from pathlib import Path
 
-try:
-    import youtube_dl
-except:
-    print('installing youtube_dl')
-    os.system('pip install youtube-dl')
-    import youtube_dl
-
 
 def download_youtube_video(youtube_id, youtube_format, youtube_file):
+    try:
+        import youtube_dl
+    except:
+        print('installing youtube_dl')
+        os.system('pip install youtube-dl')
+        import youtube_dl
+
     if not Path(youtube_file).exists():
         youtube_url = f'https://www.youtube.com/watch?v={youtube_id}'
         print(f'downloading {youtube_url}')
         os.system(f'youtube-dl {youtube_url} -f {youtube_format} -o video.mp4')
     else:
         print(f'youtube video already present')
+
+
