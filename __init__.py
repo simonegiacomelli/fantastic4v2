bed5a149e2ae6e9a60a92b81ae44987fbd15ae78
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-05-27 10:30:54 +0200

    smoother simplerpcpy experience

--- __init__.py
+++ __init__.py
@@ -1,1 +1,19 @@
-print('called!', __file__)
+from datetime import datetime
+from pathlib import Path
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print('called!', __file__, )
+print("Current Time =", current_time)
+
+import sys
+
+
+def add_sys_path(p):
+    if p not in sys.path:
+        sys.path.insert(0, p)
+
+
+add_sys_path(str(Path(__file__).parent))
+print(sys.path)
