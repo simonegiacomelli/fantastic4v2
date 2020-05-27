2a8bbb3c6879b5a8945bc3f263ee7f7875b73668
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-05-27 10:38:00 +0200

    smoother simplerpcpy experience

---
+++ s01/utils/__init__.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+
+try:
+    import youtube_dl
+except:
+    print('installing youtube_dl')
+    os.system('pip install youtube-dl')
+    import youtube_dl
+
+
+def download_youtube_video(youtube_id, youtube_format, youtube_file):
+    if not Path(youtube_file).exists():
+        youtube_url = f'https://www.youtube.com/watch?v={youtube_id}'
+        print(f'downloading {youtube_url}')
+        os.system(f'youtube-dl {youtube_url} -f {youtube_format} -o video.mp4')
+    else:
+        print(f'youtube video already present')
